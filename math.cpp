2c5177ed277120c24ba78bfbe944bf86fcb704ed
Henrique Dias <hd9business@gmail.com>
2023-09-20 23:33:16 +0100

    math

---
+++ math.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+    cout << fmax(3,10) << endl;
+    cout << fmin(3,10) << endl;
+    cout << sqrt(4) << endl;
+
+    return 0;
+}
