c092650a80515d97eaa7548ec72a21389f5d645e
Henrique Dias <hd9business@gmail.com>
2023-09-20 22:52:06 +0100

    strings

---
+++ strings.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    cout << "Caloirada!\n";
+    cout << "ZEROOO" << endl;
+
+    string nome;
+    nome = "Henrique";
+
+    cout << nome.length() << endl;
+    cout << nome[0] << endl;
+
+    nome[0] = 'M';
+    cout << nome;
+
+    return 0;
+}
