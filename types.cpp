f8b5fa7b50e25970571bbbc0406270f0981bb6ff
hd9s1lk <104395712+hd9s1lk@users.noreply.github.com>
2023-09-20 22:30:50 +0100

    Add files via upload

---
+++ types.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    char grade = 'A';
+    string phrase = "bababooey";
+    int age = 20;
+    float nr = 3.14;
+    double nr2 = 2.5;
+    bool statement = true;
+    
+
+
+    return 0;
+
+}
---
+++ variables.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    string Character_Name;
+    int Character_Age;
+
+    Character_Name = "Quinho";
+    Character_Age = 20;
+
+    cout << "Sou o "<< Character_Name << endl; 
+    cout << "e tenho "<< Character_Age << " anos :)" << endl;
+
+    Character_Name = "Joana";
+    Character_Age = 18;
+    cout << "Já a "<< Character_Name << endl; 
+    cout << "tem só "<< Character_Age << " anos :)" << endl;
+
+}
