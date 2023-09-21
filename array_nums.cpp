b1b11cfe997d9a7fba11ac83b7d71dc179b11d7a
Henrique Dias <hd9business@gmail.com>
2023-09-21 14:11:32 +0100

    array_nums

---
+++ array_nums.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+
+using namespace std;
+
+int main(){
+
+    int num[] = {2,5,8};
+
+    num[1] = 12;
+
+    cout << num[0] << endl;
+    cout << num[1] << endl;
+
+    system("pause");
+    return 0;
+}
