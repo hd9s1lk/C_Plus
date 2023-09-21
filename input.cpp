3fa2d748782c1746b35ae0d552134c5e49699468
Henrique Dias <hd9business@gmail.com>
2023-09-21 13:29:27 +0100

    input

---
+++ input.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int age;
+    cout << "Insira a sua idade: \n";
+    cin >> age;
+
+    cout << "Tu tens " << age << "anos :)" << endl;
+    
+    return 0;
+}
