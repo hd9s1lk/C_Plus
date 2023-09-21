2b8b88950e7c38d80f28d94ec2741e093f130d0f
Henrique Dias <hd9business@gmail.com>
2023-09-21 14:10:09 +0100

    array_nome

---
+++ array_nome.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+
+using namespace std;
+
+int main(){
+    char nome[30];
+
+    cout << "Insira o seu nome: ";
+    cin >> nome;
+
+    cout << "O teu nome e: " << nome;
+
+    system("pause");
+    return 0;
+}
