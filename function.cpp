803c852fbfc56c2b7dcc2f3e18d4974449d86ca7
Henrique Dias <hd9business@gmail.com>
2023-09-21 14:23:14 +0100

    function

---
+++ function.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+
+using namespace std;
+
+void chao(string name){
+    cout << "Para o chao caloiro: " << name;
+
+}
+
+int main(){
+
+    chao("Manuel");
+
+    system("pause");
+    return 0;
+}
